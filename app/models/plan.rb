25754d6d8a7e7caa1b3f09e1e14b899af0bd059c
Christian Dyl <christian.dyl@outlook.com>
2015-11-29 15:42:46 +0200

    small fix in plan

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,7 +1,7 @@
 class Plan < ActiveRecord::Base
   include ApiConverter
 
-  attr_api [:id, :name, :max_players, :host_id]
+  attr_api [:id, :name, :max_players, :price, :host_id]
 
   belongs_to :game
   belongs_to :host
