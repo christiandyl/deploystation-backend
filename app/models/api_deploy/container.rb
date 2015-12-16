abee400c62a0e546c461b9d9684a1bc284d78e0f
Christian Dyl <christian.dyl@outlook.com>
2015-12-16 18:40:46 +0200

    new change

--- app/models/api_deploy/container.rb
+++ app/models/api_deploy/container.rb
@@ -2,7 +2,7 @@ module ApiDeploy
   class Container < ActiveRecord::Base
     include ApiConverter
 
-    attr_api [:id, :status, :host_info, :plan_info, :ip, :name]
+    attr_api [:id, :status, :host_info, :plan_info, :ip, :name, :is_private]
     
     before_destroy :on_before_destroy
     
