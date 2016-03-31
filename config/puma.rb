4f4fe4a37a93596877c46c6de50ff08e179da843
Christian Dyl <christian@deploystation.com>
2016-03-31 21:47:37 +0200

    push

--- config/puma.rb
+++ config/puma.rb
@@ -4,7 +4,7 @@ workers 2
 # Min and Max threads per worker
 threads 1, 6
 
-app_dir = File.expand_path("../..", __FILE__)
+app_dir = File.expand_path("/var/www/api.deploystation.com/current", __FILE__)
 shared_dir = "#{app_dir}/tmp"
 
 # Default to production
