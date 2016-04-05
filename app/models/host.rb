cb45574706175eb4b5e4cc98017e11e69606a4ad
Christian Dyl <christian@deploystation.com>
2016-04-05 12:38:59 +0200

    ssh key

--- app/models/host.rb
+++ app/models/host.rb
@@ -40,7 +40,8 @@ class Host < ActiveRecord::Base
     port = nil
     
     unless ip == "127.0.0.1"
-      Net::SSH.start(ip, host_user) do |ssh|
+      keys = [ssl_path.join('id_rsa').to_s]
+      Net::SSH.start(ip, host_user, keys: keys) do |ssh|
         out  = ssh.exec!("sh /opt/scripts/get_free_port/run.sh")
         port = out.split("\n").first
       end
