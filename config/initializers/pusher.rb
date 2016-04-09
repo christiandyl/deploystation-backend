3f7ae7b654001fceb97ce18dc5b0c0aab7dc1d98
Christian Dyl <christian@deploystation.com>
2016-04-09 17:37:07 +0200

    pusher port

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,5 +1,5 @@
 Pusher.host   = Settings.pusher.host
-Pusher.port   = Settings.pusher.port
+Pusher.port   = Settings.pusher.port if Settings.pusher.port == "443"
 Pusher.app_id = Settings.pusher.app_id
 Pusher.key    = Settings.pusher.key
 Pusher.secret = Settings.pusher.secret
