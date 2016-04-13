a9bd9461f3b1e70f4eda46e5cc87bfd43780a3e8
Christian Dyl <christian@deploystation.com>
2016-04-13 16:25:55 +0200

    new fix

--- config/application.rb
+++ config/application.rb
@@ -51,6 +51,20 @@ module Node
       end if File.exists?(env_file)
     end
     
+    config.action_mailer.smtp_settings = {
+      :address              => Settings.mandrill.smtp_address,
+      :authentication       => :plain,
+      :domain               => Settings.mandrill.smtp_domain,
+      :enable_starttls_auto => true,
+      :password             => Settings.mandrill.smtp_password,
+      :port                 => "587",
+      :user_name            => Settings.mandrill.smtp_username
+    }
+
+    config.action_mailer.default_url_options = {
+      :host => Settings.mandrill.smtp_domain
+    }
+    
     # custom logger formatter
     class LoggerFormatter
       USE_HUMOROUS_SEVERITIES = true
