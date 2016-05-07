443b62f1ab2b107dbc525525bdcbfda1a5e1cf84
Christian Dyl <christian@deploystation.com>
2016-05-07 13:32:02 +0200

    deploy 2016-05-07 13:32:01 +0200

--- app/workers/api_deploy/container_checker_worker.rb
+++ app/workers/api_deploy/container_checker_worker.rb
@@ -4,15 +4,17 @@ module ApiDeploy
 
     def perform(limit, offset)
       Container.online.limit(limit).offset(offset).each  do |c|
-        container = Container.class_for(c.game.sname).find(c.id)
-        hs = container.players_online(true)
+        container = Container.class_for(c.game.sname).find_by_id(c.id)
+        unless container.nil?
+          hs = container.players_online(true)
 
-        container.players = hs[:players_online].to_s + "/" + hs[:max_players].to_s
+          container.players = hs[:players_online].to_s + "/" + hs[:max_players].to_s
         
-        Pusher.trigger("container-#{container.id}", "players_online", {
-          :success => true,
-          :result  => hs
-        })
+          Pusher.trigger("container-#{container.id}", "players_online", {
+            :success => true,
+            :result  => hs
+          })
+        end
 
       end
     end
