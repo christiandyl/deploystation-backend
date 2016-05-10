f107fae0e0fe4c23c7449e31181fa900ffba0b3d
Christian Dyl <christian@deploystation.com>
2016-05-10 13:56:43 +0200

    deploy 2016-05-10 13:56:42 +0200

--- app/workers/api_deploy/periodic/container_players_online_worker.rb
+++ app/workers/api_deploy/periodic/container_players_online_worker.rb
@@ -1,37 +1,38 @@
-module ApiDeploy::Periodic
-  class ContainerPlayersOnlineWorker
-    include Sidekiq::Worker
+module ApiDeploy
+  module Periodic
+    class ContainerPlayersOnlineWorker
+      include Sidekiq::Worker
 
-    sidekiq_options unique: :all, queue: 'critical'
+      sidekiq_options unique: :all, queue: 'critical'
 
-    def perform(limit, offset)
-      ::ApiDeploy::Container.online.limit(limit).offset(offset).each  do |c|
-        container = Container.class_for(c.game.sname).find_by_id(c.id)
-        unless container.nil?
-          hs = container.players_online(true)
+      def perform(limit, offset)
+        Container.online.limit(limit).offset(offset).each  do |c|
+          container = Container.class_for(c.game.sname).find_by_id(c.id)
+          unless container.nil?
+            hs = container.players_online(true)
 
-          container.players = hs[:players_online].to_s + "/" + hs[:max_players].to_s
+            container.players = hs[:players_online].to_s + "/" + hs[:max_players].to_s
         
-          channel_name = "container-#{container.id}"
+            channel_name = "container-#{container.id}"
         
-          # TODO channel exists validation for slanger
-          begin
-            channels = Pusher.channels
-            channel_exists = !channels[:channels][channel_name].nil? rescue false
-          rescue
-            channel_exists = true
-          end
+            # TODO channel exists validation for slanger
+            begin
+              channels = Pusher.channels
+              channel_exists = !channels[:channels][channel_name].nil? rescue false
+            rescue
+              channel_exists = true
+            end
           
-          if channel_exists
-            Pusher.trigger(channel_name, "players_online", {
-              :success => true,
-              :result  => hs
-            })
+            if channel_exists
+              Pusher.trigger(channel_name, "players_online", {
+                :success => true,
+                :result  => hs
+              })
+            end
           end
-        end
 
+        end
       end
     end
-
   end
 end
