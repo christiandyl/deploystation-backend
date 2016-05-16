34cb69eec37c2d9eaaa403cc520ea3e22819413a
Christian Dyl <christian@deploystation.com>
2016-05-16 18:06:27 +0200

    deploy 2016-05-16 18:06:25 +0200

--- app/workers/api_deploy/periodic/container_daily_stat_worker.rb
+++ app/workers/api_deploy/periodic/container_daily_stat_worker.rb
@@ -6,10 +6,15 @@ module ApiDeploy
       sidekiq_options unique: :all, queue: 'low'
 
       def perform(limit, offset)
+        total_time = 0
         Container.all.each do |c|
           container = Container.class_for(c.game.sname).find(c.id)
-          container.calculate_stats
+          stats = container.calculate_stats
+          
+          total_time += (stats[:total_gaming_time] || 0) rescue 0
         end
+        total_minutes = total_time / 60
+        ApiDeploy::Helper::slack_ping("Total gaming time today (#{Time.now.to_s}) is #{total_minutes.to_s} minutes")
       end
 
     end
