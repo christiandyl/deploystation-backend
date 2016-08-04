93b94137ee5ce6a07797d8bacac55d2dc32a1c26
Christian Dyl <christian@deploystation.com>
2016-08-04 17:49:51 +0300

    deploy 2016-08-04 17:49:51 +0300

--- app/models/user.rb
+++ app/models/user.rb
@@ -74,7 +74,7 @@ class User < ActiveRecord::Base
       confirmation_required: confirmation_required,
     }
     if layers.include? :private
-      h[:credits] = credits.round(5)
+      h[:credits] = credits.round(2)
     end
 
     h
