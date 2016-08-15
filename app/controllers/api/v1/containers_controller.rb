ea6c7a6121eb8ca75450033b2ea9735af13dbf78
Christian Dyl <christian@deploystation.com>
2016-08-15 11:47:44 +0300

    deploy 2016-08-15 11:47:44 +0300

--- app/controllers/api/v1/containers_controller.rb
+++ app/controllers/api/v1/containers_controller.rb
@@ -6,7 +6,7 @@ module Api
       before_filter :get_container, :except => [:index, :shared, :create, :bookmarked, :popular, :search]
       before_action :check_permissions, :except => [:index, :shared, :create, :destroy, :bookmarked, :popular, :show, :search, :players_online]
       before_action :check_super_permissions, :only => [:destroy]
-      before_filter :check_is_active, :except => [:index, :shared, :create, :bookmarked, :popular, :search, :show, :destroy]
+      before_filter :check_is_active, :except => [:index, :shared, :create, :bookmarked, :popular, :search, :show, :destroy, :referral_token]
 
       ##
       # Get popular containers
