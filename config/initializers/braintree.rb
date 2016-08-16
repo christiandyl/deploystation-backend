e85a4ea88d88e2112f3095ff97dfd627e9a6df86
Christian Dyl <christian@deploystation.com>
2016-08-16 18:41:19 +0300

    deploy 2016-08-16 18:41:19 +0300

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,7 +1,10 @@
 # Braintree::Configuration.environment = :sandbox unless Rails.env.production?
 unless Rails.env.production?
-  Braintree::Configuration.environment = :sandbox if Settings.braintree.sandbox
+  Braintree::Configuration.environment = Settings.braintree.sandbox ? :sandbox : :production
+else
+  Braintree::Configuration.environment = :production
 end
+
 Braintree::Configuration.merchant_id = Settings.braintree.merchant_id
 Braintree::Configuration.public_key  = Settings.braintree.public_key
 Braintree::Configuration.private_key = Settings.braintree.private_key
