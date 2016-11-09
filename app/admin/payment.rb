a62b9fb147f97f03ca4f042a5b19355f99ea97c7
Christian Dyl <christian@deploystation.com>
2016-11-09 16:29:23 +0200

    deploy 2016-11-09 16:29:23 +0200

--- app/admin/payment.rb
+++ app/admin/payment.rb
@@ -1,2 +1,10 @@
 ActiveAdmin.register Payment do
+  index do
+    column :id
+    column :type
+    column 'Type' { |p| p.type }
+    column 'Status' { |p| p.status }
+    column :created_at
+    actions
+  end
 end
